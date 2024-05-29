70ce5570bb9d7f423da5d76a89ea464146a533fe
JamesGwapogi <167041980+JamesGwapogi@users.noreply.github.com>
2024-05-29 14:08:58 +0800

    Add files via upload

---
+++ LaundryManagementSystem/LoginHomepage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaundryManagementSystem
+{
+    public partial class LoginHomepage : Form
+    {
+        public LoginHomepage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn_Click(object sender, EventArgs e)
+        {
+            Dashboard DB = new Dashboard();
+            DB.Show();
+            this.Hide();
+        }
+
+        private void ForgotPassword_Click(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void CreateAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
